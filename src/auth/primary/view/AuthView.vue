02884a82c7eb9edbc9d3c71bed4612185b8bb829
ayho <y.houssin01@gmail.com>
2022-09-27 09:33:05 +0200

    first step

---
+++ src/auth/primary/view/AuthView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="auth">
+    <Logo />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import Logo from "@/common/primary/components/Logo.vue";
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/common/primary/components/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="logo">
+    <h1>Spare Shoes</h1>
+  </div>
+</template>
+<style lang="scss">
+.logo {
+  h1 {
+    font-family: 'Inter', sans-serif;
+  }
+}
+</style>
